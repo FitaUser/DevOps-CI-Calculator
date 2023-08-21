8a9d0b3e472ce682ba26a6952f654918ab2e2f73
FitaUser <pavels.grjozs@fita.lv>
2023-08-21 20:24:50 +0300

    return only summ

--- calculator.py
+++ calculator.py
@@ -2,12 +2,5 @@
 def add(x, y):
     return x + y
     
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-    
-
-
 print(num1, "+", num2, "=", add(num1, num2))
 
-print(num1, "-", num2, "=", subtract(num1, num2))
