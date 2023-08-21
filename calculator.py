711ace4b04e45265f459a370e2fa47b764dc139c
FitaUser <pavels.grjozs@fita.lv>
2023-08-21 20:25:53 +0300

    multiply

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 # This function adds two numbers
 def add(x, y):
     return x + y
-    
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+
 print(num1, "+", num2, "=", add(num1, num2))
 
+print(num1, "*", num2, "=", multiply(num1, num2))
