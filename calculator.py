1d69811f0e30ea6436726b216a69721d0a253b25
FitaUser <pavels.grjozs@fita.lv>
2023-08-21 20:20:35 +0300

    subtract

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+    
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+    
+
+
+print(num1, "+", num2, "=", add(num1, num2))
+
+print(num1, "-", num2, "=", subtract(num1, num2))
