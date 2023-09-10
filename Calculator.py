b7b20f02312f38c5c2394a2537ddf632abef51b8
FitaUser <pavels.grjozs@fita.lv>
2023-09-10 21:06:15 +0300

    Feature_1: Add functions and untit test.

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,13 @@ Study purpose
 Topic: Continuous integration
 
 """
+
+# unit test for this function test_add.py
+def add(variable_x, variable_y):
+    """
+    
+    Add two numbers together.
+    
+    """
+    return variable_x + variable_y
+
