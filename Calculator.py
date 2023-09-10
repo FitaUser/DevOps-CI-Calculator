5fce745c76d309e4b4117858ddf9216976b71447
FitaUser <pavels.grjozs@fita.lv>
2023-09-10 21:58:51 +0300

    Feature_2: Subtraction function

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,12 @@ Study purpose
 Topic: Continuous integration
 
 """
+
+# unit test for this function test_subtraction.py
+def subtraction(variable_x, variable_y):
+    """
+    
+    Subtraction two numbers.
+    
+    """
+    return variable_x - variable_y
